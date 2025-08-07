2385a464807fae4e9032446eeb1a4100117b646d
Blankeye-zero <60312301+Blankeye-zero@users.noreply.github.com>
2025-08-08 00:19:26 +0530

    enhace: refactored the code

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,16 +1,19 @@
-class StringCalculator{
-    
-    int add(String numbers){
-        //Input String of comma seperated numbers
-        //Output an integer, sum of numbers
-        if(numbers.isEmpty){
-            return 0;
-        }
-        List<int> givenNumbers = numbers.split(',').map((stringNumber) => int.parse(stringNumber)).toList();
-        int sum = 0;
-        for (int number in givenNumbers){
-            sum = sum+number;
-        }
-        return sum;
+class StringCalculator {
+  int add(String numbers) {
+    //Input String of comma seperated numbers
+    //Output an integer, sum of numbers
+    int sum = 0;
+    if (numbers.isEmpty) {
+      return sum;
     }
+    List<int> givenNumbers = numbers
+        .split(',')
+        .map((stringNumber) => int.parse(stringNumber))
+        .toList();
+
+    for (int number in givenNumbers) {
+      sum = sum + number;
+    }
+    return sum;
+  }
 }
