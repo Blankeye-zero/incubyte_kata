ba563c98cd4592b42e66ec28b2731b9829477321
Blankeye-zero <60312301+Blankeye-zero@users.noreply.github.com>
2025-08-08 00:08:19 +0530

    test: fail type String is not a subtype of type int in typecast

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,11 @@ class StringCalculator{
     int add(String numbers){
         //Input String of comma seperated numbers
         //Output an integer, sum of numbers
-        return 0;
+        List<int> givenNumbers = numbers.split(',').map((e) => e as int).toList();
+        int sum = 0;
+        for (int number in givenNumbers){
+            sum = sum+number;
+        }
+        return sum;
     }
 }
