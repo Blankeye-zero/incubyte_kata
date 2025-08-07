5cbce531ff9f3c7ee373627309ee181c98477d86
Blankeye-zero <60312301+Blankeye-zero@users.noreply.github.com>
2025-08-08 00:13:02 +0530

    test: Passed for both empty string and single value test cases

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,9 @@ class StringCalculator{
     int add(String numbers){
         //Input String of comma seperated numbers
         //Output an integer, sum of numbers
+        if(numbers.isEmpty){
+            return 0;
+        }
         List<int> givenNumbers = numbers.split(',').map((stringNumber) => int.parse(stringNumber)).toList();
         int sum = 0;
         for (int number in givenNumbers){
